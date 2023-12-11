27a09f10bab6f9cf1d707f13ae8c52f0da657df6
IngoForrerNuila <ingoproductions@gmail.com>
2023-12-11 13:54:50 +0100

    modificasiones-animacion-7

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 
-//-------cuando parrafo no es visible se oculta y cuando es visible vuelve a aparecer-----//
+//------------------corneredlogo efecto scroll hacia arriba---------------//
+
+document.addEventListener('DOMContentLoaded', function() {
+  var corneredLogo = document.querySelector('#logo-mini');
+  var section1 = document.getElementById('section1');
+
+  corneredLogo.addEventListener('click', function() {
+    section1.scrollIntoView({ behavior: 'smooth' });
+  });
+});
+
+
+//-------cuando parrafo section2-p no es visible se oculta y cuando es visible vuelve a aparecer-----//
 document.addEventListener("DOMContentLoaded", function () {
   const section2 = document.getElementById('section2');
   const section2_p = document.getElementById('section2_p');
@@ -19,6 +31,49 @@ document.addEventListener("DOMContentLoaded", function () {
   }, { threshold: 0.5 });
 
   observer.observe(section2);
+});
+
+//-------cuando parrafo section3-p no es visible se oculta y cuando es visible vuelve a aparecer-----//
+document.addEventListener("DOMContentLoaded", function () {
+  const section3 = document.getElementById('section3');
+  const section3_p = document.getElementById('section3_p');
+
+  const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        // Solo aplica la animación si el elemento es completamente visible
+        section3_p.style.transition = 'transform 1s ease-out';
+        section3_p.style.transform = 'translateX(0)';
+      } else {
+        // Si no es visible, agrega un desplazamiento hacia la izquierda antes de desaparecer
+        section3_p.style.transition = 'transform 1s ease-out';
+        section3_p.style.transform = 'translateX(-130%)';
+      }
+    });
+  }, { threshold: 0.5 });
+
+  observer.observe(section3);
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const section3 = document.getElementById('section3');
+  const section3_p = document.getElementById('section3_p2');
+
+  const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        // Solo aplica la animación si el elemento es completamente visible
+        section3_p2.style.transition = 'transform 1s ease-out';
+        section3_p2.style.transform = 'translateX(0)';
+      } else {
+        // Si no es visible, agrega un desplazamiento hacia la izquierda antes de desaparecer
+        section3_p2.style.transition = 'transform 1s ease-out';
+        section3_p2.style.transform = 'translateX(-130%)';
+      }
+    });
+  }, { threshold: 0.5 });
+
+  observer.observe(section3);
 });
 
 // ---texto u objeto desaparece cuando contenedor ya no es visible para re iniciar la animacion---//
@@ -63,16 +118,7 @@ document.addEventListener("DOMContentLoaded", function () {
   });*/
 
 
-//------------------corneredlogo efecto scroll hacia arriba---------------//
 
-document.addEventListener('DOMContentLoaded', function() {
-    var corneredLogo = document.querySelector('#logo-mini');
-    var section1 = document.getElementById('section1');
-  
-    corneredLogo.addEventListener('click', function() {
-      section1.scrollIntoView({ behavior: 'smooth' });
-    });
-  });
 
 
 
